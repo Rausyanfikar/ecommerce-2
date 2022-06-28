07de01e0428fe6fa227a5d77322ecb3163409200
rausyanfikar <rauzyanfikar@gmail.com>
2022-06-28 21:05:20 +0700

    login kelar

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -3,73 +3,41 @@ import Layout from '../components/Layout';
 import { useState, useEffect } from 'react';
 import { Card, CardLoading } from '../components/Card';
 import Hero from '../components/Hero';
+import axios from 'axios';
 
 const HomePages = () => {
-  const state = {
-    products: [
-      {
-        id: 1,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'Vans',
-        price: 5000,
-      },
-      {
-        id: 2,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'Yeezy',
-        price: 2000,
-      },
-      {
-        id: 3,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'Air Jordan',
-        price: 10.0,
-      },
-      {
-        id: 4,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'sepatu',
-        price: 10.0,
-      },
-      {
-        id: 5,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'sepatu',
-        price: 10.0,
-      },
-      {
-        id: 6,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'sepatu',
-        price: 10.0,
-      },
-      {
-        id: 7,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'sepatu',
-        price: 10.0,
-      },
-      {
-        id: 8,
-        img: 'https://image.tmdb.org/t/p/w500/rkpLvPDe0ZE62buUS32exdNr7zD.jpg',
-        name: 'sepatu',
-        price: 10.0,
-      },
-    ],
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProduct = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=d06c4019966aa719f29e61a7c4ad76a8&language=en-US&page=1`)
+      .then((response) => {
+        // handle success
+
+        const { results } = response.data;
+        setProduct(results);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
   };
 
-  //   if (loading) {
-  //     return <CardLoading />;
-  //   } else {
-  return (
-    <Layout>
-      <Hero />
-      <div className="grid grid-flow-row auto-rows-max grid-cols-2 md:grid-cols-4 lg:grid-cols-4 m-2 gap-3">
-        {state.products.map((item) => (
-          <Card key={item.id} title={item.title} image={item.img} price={item.price} product={item.name} />
-        ))}
-      </div>
-    </Layout>
-  );
+  if (loading) {
+    return <CardLoading />;
+  } else {
+    return (
+      <Layout>
+        <Hero />
+        <div className="grid grid-flow-row auto-rows-max grid-cols-2 md:grid-cols-4 lg:grid-cols-4 m-2 gap-3">
+          {state.products.map((item) => (
+            <Card key={item.id} title={item.title} image={item.img} price={item.price} product={item.name} />
+          ))}
+        </div>
+      </Layout>
+    );
+  }
 };
 export default HomePages;
